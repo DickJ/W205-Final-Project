4309f036c8a8357f2f2a7d5ba1fe456ddab2e6e0
joanqiu <qyjoan@gmail.com>
2015-04-04 12:53:39 -0400

    add part of speech analysis and remove word position and tf from output

--- index/inverted_index_mr.py
+++ index/inverted_index_mr.py
@@ -1,45 +1,82 @@
-# -*- coding: utf-8 -*-
 import mrjob
 from mrjob.job import MRJob
-from mrjob.step import MRStep
-import json
+import ast
+import nltk
+from nltk import PorterStemmer
+from nltk.tokenize import RegexpTokenizer, word_tokenize
 
+def clean_str(line):
+    line = line.strip()
+    line = line.replace('\\t','')
+    line = line.replace('\\n','')
+    line = line.replace('\\r','')
+    return line
+
+measures = ['ounce','ounces','cup','cups','pound','pounds','kilos',\
+            'grams','gram','kilo','bag','bags','teaspoon',\
+            'teaspoons','tablespoon','tablespoons','tbsp','tbsps',\
+            'lbs','kg','kilogram','kilograms','can','cans',\
+            'tsp','tsps','oz','pint','pt','pints','pack','packs','packed']
+
+english_sw = nltk.corpus.stopwords.words('english')
+
+methods = ['grounded','crushed', 'chopped']
+
+def extract_key_ingred(ingredient):
+    ingredient = ingredient.strip().lower()
+    #print "original ingredients {}".format(ingredient)
+    tokenizer = RegexpTokenizer(r'\w+')
+    tkn = tokenizer.tokenize(ingredient)
+    ps = PorterStemmer()
+    tkn = [ps.stem(t) for t in tkn if t not in measures and t not in methods and t not in english_sw]
+    #print tkn
+    tagged = nltk.pos_tag(tkn)
+    #print tagged
+    key_words = [w for w,tag in tagged if tag != 'LS' and tag != 'CD']
+    np = [w for w,tag in tagged if tag == 'NN'] # noun phrase
+    if len(np) > 0 and np not in key_words:
+        key_words.append(' '.join(np))
+    return key_words
+    
 class Indexing(MRJob):
     
-    def configure_options(self):
-        super(Indexing, self).configure_options()
-        self.add_passthrough_option('--source',\
-                                    help="Source location of recipe data (s3 or local)")
-    
-    def index_mapper(self, _, line):
+    def mapper(self, _, line):
         if len(line) > 0:
-            docid, doc = line.split('\t')
-            doc = json.loads(doc)
-            tf = {}
-            tpos = {}
-            pos = 0
-            for term in doc['contents'].split():
-                tf[term] = tf.get(term,0) + 1
-                if term in tpos:
-                    tpos[term].append(pos)
-                else:
-                    tpos[term] = [pos]
-                pos += 1
-            for term in tf:
-                yield term, (docid, tf[term], tpos[term], pos+1)
+            l = clean_str(line)
+            data = ast.literal_eval(l)
+            url, ingredients, title = data['url'],data['ingred'],data['title']
+            #tf = {}
+            #tpos = {}
+            #pos = 0
+            for ingred in ingredients:
+                for term in extract_key_ingred(ingred):
+                    yield term, (url, title)
+                    #tf[term] = tf.get(term,0) + 1
+                    #if term in tpos:
+                    #    tpos[term].append(pos)
+                    #else:
+                    #    tpos[term] = [pos]
+                    #pos += 1
+                #for term in tf:
+                    #yield term, (url, tf[term], tpos[term], pos+1)
 
-    def index_reducer(self, term, postings):
+    def combiner(self, term, postings):
+        p = []
+        for post in postings:
+            p.append(post)
+        yield term, p
+
+    def reducer(self, term, postings):
         p = []
         for posting in postings:
             p.append(posting)
         yield term, p
     
-    def steps(self):
-        return [#MRStep(mapper=self.normalize),
-                MRStep(mapper=self.index_mapper,
-                       reducer=self.index_reducer)
-                ]
+    #def steps(self):
+    #    return [MRStep(mapper=self.index_mapper,
+    #                   combiner = self.index_combiner,
+    #                   reducer=self.index_reducer)
+    #            ]
                        
 if __name__ == '__main__':
     Indexing.run()
-
