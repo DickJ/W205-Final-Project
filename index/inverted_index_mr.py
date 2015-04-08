fbbf5c8a356e89747fd6b963ba26ec5bb441bed5
joanqiu <qyjoan@gmail.com>
2015-04-07 23:19:28 -0400

    changing store index to mongo

--- index/inverted_index_mr.py
+++ index/inverted_index_mr.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mrjob
 from mrjob.job import MRJob
 import ast
@@ -6,6 +5,7 @@ import re
 import nltk
 from nltk import PorterStemmer
 from nltk.tokenize import RegexpTokenizer
+from recipewords import fullIngredients, measures, methods
 
 def clean_str(line):
     line = line.strip()
@@ -16,6 +16,7 @@ def clean_str(line):
 
 def patch_ingred(ingredient):
     ingredient = ingredient.replace("&#174","")
+    ingredient = re.sub(r'-','',ingredient)
     ingredient = re.sub(r'[\xf1]','n',ingredient)
     ingredient = re.sub(r'[^\x00-\x7F]',' ', ingredient)
     ingredient = re.sub(r'&nbsp','',ingredient)
@@ -23,19 +24,7 @@ def patch_ingred(ingredient):
     ingredient = re.sub(r'&nbsp|&rarr|&larr','',ingredient)
     return ingredient
 
-measures = ['ounce','ounces','cup','cups','pound','pounds','kilos',\
-            'grams','gram','kilo','bag','bags','teaspoon',\
-            'teaspoons','tablespoon','tablespoons','tbsp','tbsps',\
-            'lbs','kg','kilogram','kilograms','can','cans',\
-            'tsp','tsps','oz','pint','pt','pints','pack','packs','packed',\
-            'pinch']
-
 english_sw = nltk.corpus.stopwords.words('english')
-
-methods = ['grounded','crushed','chopped', 'cored', 'peeled', 'sliced',\
-           'squeezed','diced','divided','softened','thawed','needed',\
-           'drained','rinsed','beaten','husked','cleaned','mashed','melted',\
-           'dried','processed','grated','frying','chopp','chuncked','chunk']
 
 def extract_key_ingred(ingredient):
     ingredient = ingredient.strip().lower()
@@ -43,15 +32,21 @@ def extract_key_ingred(ingredient):
     #print "original ingredients {}".format(ingredient)
     tokenizer = RegexpTokenizer(r'\w+')
     tkn = tokenizer.tokenize(ingredient)
+    ingredientList = list(set(fullIngredients.split('\n')))
+    tkn = [t for t in tkn if \
+           t in ingredientList and \
+           t not in measures and \
+           t not in methods and \
+           t not in english_sw]
     ps = PorterStemmer()
-    tkn = [ps.stem(t) for t in tkn if t not in measures and t not in methods and t not in english_sw]
-    tagged = nltk.pos_tag(tkn)
-    key_words = [w for w,tag in tagged if tag != 'LS' and tag != 'CD']
-    np = [w for w,tag in tagged if tag == 'NN'] # noun phrase
-    np = ' '.join(np)
-    if np not in key_words and len(np) > 0 and len(np.split()) <= 3:
-        key_words.append(np)
-    return key_words
+    tkn = [ps.stem(t) for t in tkn]
+    #tagged = nltk.pos_tag(tkn)
+    #key_words = [w for w,tag in tagged if tag != 'LS' and tag != 'CD']
+    #np = [w for w,tag in tagged if tag == 'NN'] # noun phrase
+    #np = ' '.join(np)
+    #if np not in key_words and len(np) > 0 and len(np.split()) <= 3:
+    #    key_words.append(np)
+    return tkn
     
 class Indexing(MRJob):
     
@@ -59,13 +54,13 @@ class Indexing(MRJob):
         if len(line) > 0:
             l = clean_str(line)
             data = ast.literal_eval(l)
-            url, ingredients, title = data['url'],data['ingred'],data['title']
+            uid, ingredients = data['_id'],data['ingred']
             #tf = {}
             #tpos = {}
             #pos = 0
             for ingred in ingredients:
                 for term in extract_key_ingred(ingred):
-                    yield term, (url, title)
+                    yield (term, uid)
                     #tf[term] = tf.get(term,0) + 1
                     #if term in tpos:
                     #    tpos[term].append(pos)
@@ -89,50 +84,3 @@ class Indexing(MRJob):
                        
 if __name__ == '__main__':
     Indexing.run()
-=======
-# -*- coding: utf-8 -*-
-import mrjob
-from mrjob.job import MRJob
-from mrjob.step import MRStep
-import json
-
-class Indexing(MRJob):
-    
-    def configure_options(self):
-        super(Indexing, self).configure_options()
-        self.add_passthrough_option('--source',\
-                                    help="Source location of recipe data (s3 or local)")
-    
-    def index_mapper(self, _, line):
-        if len(line) > 0:
-            docid, doc = line.split('\t')
-            doc = json.loads(doc)
-            tf = {}
-            tpos = {}
-            pos = 0
-            for term in doc['contents'].split():
-                tf[term] = tf.get(term,0) + 1
-                if term in tpos:
-                    tpos[term].append(pos)
-                else:
-                    tpos[term] = [pos]
-                pos += 1
-            for term in tf:
-                yield term, (docid, tf[term], tpos[term], pos+1)
-
-    def index_reducer(self, term, postings):
-        p = []
-        for posting in postings:
-            p.append(posting)
-        yield term, p
-    
-    def steps(self):
-        return [#MRStep(mapper=self.normalize),
-                MRStep(mapper=self.index_mapper,
-                       reducer=self.index_reducer)
-                ]
-                       
-if __name__ == '__main__':
-    Indexing.run()
-
->>>>>>> master
--- index/store_index.py
+++ index/store_index.py
@@ -1,8 +1,9 @@
 import boto
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 import re
 import ast
 from settings import *
+import logging
 
 
 client = MongoClient(MONGODB_URI)
@@ -10,19 +11,36 @@ pattern = re.compile('out/part-*')
 db = client.get_default_database()
 recipe_index = db[index_db]
 
+MAX_AUTO_RECONNECT_ATTEMPTS = 5
+
 conn = boto.connect_s3(AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY)
-bk = conn.get_bucket('joan-qiu-w205-finalproject-index')
+bk_name = 'joan-qiu-w205-finalproject-index'
+bk = conn.get_bucket(bk_name)
+
+def download_from_s3(key):
+    print "downloading from s3://{}/{}...".format(bk_name,key.key)
+    f = key.get_contents_as_string()
+    print "Finish downloading."
+    lines = f.strip().split('\n')
+    del(f)
+    for line in lines:
+        yield line
+
 keys = bk.get_all_keys()
 for k in keys:
     if re.match(pattern, k.key):
-        print "downloading {}...".format(k.key)
-        f = k.get_contents_as_string()
-        print "Finish downloading."
-        for line in f.strip().split('\n'):
+        for line in download_from_s3(k):
             ingred, val = line.split('\t')
             ingred = ast.literal_eval(ingred)
             psts = ast.literal_eval(val)
-            for ps in psts:
-                posts = [{'url':p[0], 'title':p[1]} for p in ps]
-                data = {'ingredient':ingred, 'postinglist': posts}
-                recipe_index.insert(data)
+            data = dict(zip(['ingredient','postinglist'],[ingred,psts]))
+            for attempt in range(5):
+                try:
+                    recipe_index.insert(data)
+                    break
+                except errors.AutoReconnect:
+                    wait_t = 0.5 * pow(2, attempt)
+                    logging.warning("""
+                            PyMongo auto-reconnecting...Attempt {}.
+                            Waiting {} seconds.""".format(attempt, wait_t))
+                    time.sleep(wait_t)              
