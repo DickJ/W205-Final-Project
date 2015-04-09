631c66f8859d16f3e249f6b9a9c810043cb034df
rich <richard-johnson@ischool.berkeley.edu>
2015-04-09 12:14:14 -0400

    food52.com scraper complete

---
+++ scrape/scrapy/recipeScrapy/spiders/food52.py
@@ -0,0 +1,28 @@
+__author__ = 'Rich Johnson'
+
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
+from ..items import recipeItem
+import re
+
+class Food52Spider(CrawlSpider):
+    name = 'food52'
+    allowed_domains = ['food52.com']
+    start_urls = ["http://food52.com/recipes"]
+    rules = (
+        Rule(LinkExtractor(allow=".*food52\.com/recipes/\d+-.*"),
+              callback='parse_item'),
+        Rule(LinkExtractor(allow=".*food52\.com/recipes\?page=\d+"))
+    )
+
+    def __init__(self):
+        super(Food52Spider, self).__init__()
+        self.seen_recipes = set()
+
+    def parse_item(self, response):
+        item = recipeItem()
+        item['url'] = response.url
+        item['url'] = re.sub("\?.*", "", item['url'])
+        if item['url'] not in self.seen_recipes:
+            self.seen_recipes.add(item['url'])
+            return item
