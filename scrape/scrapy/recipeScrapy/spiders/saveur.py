30e884596e00de0550a85475a632695adb423fb2
rich <richard-johnson@ischool.berkeley.edu>
2015-04-09 13:52:14 -0400

    saveur.com scraper complete

---
+++ scrape/scrapy/recipeScrapy/spiders/saveur.py
@@ -0,0 +1,29 @@
+__author__ = 'Rich Johnson'
+
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
+from ..items import recipeItem
+import re
+
+class SaveurSpider(CrawlSpider):
+    name = 'saveur'
+    allowed_domains = ['saveur.com']
+    start_urls = ["http://www.saveur.com/recipe-collections?sort=1&page=0"]
+    rules = (
+        Rule(LinkExtractor(allow=".*saveur\.com/article/(r|R)ecipes/[\w\d-]"),
+              callback='parse_item'),
+        Rule(LinkExtractor(allow=[".*saveur\.com/recipe-collections\?page=\d+&sort=1",
+                                  ".*saveur\.com/article/.*"]))
+    )
+
+    def __init__(self):
+        super(SaveurSpider, self).__init__()
+        self.seen_recipes = set()
+
+    def parse_item(self, response):
+        item = recipeItem()
+        item['url'] = response.url
+        item['url'] = re.sub("\?.*", "", item['url'])
+        if item['url'] not in self.seen_recipes:
+            self.seen_recipes.add(item['url'])
+            return item
