98ab019b4e0bce2646a2570be3768049b46685bf
DickJ <richard.johnson@ischool.berkeley.edu>
2015-04-20 13:34:41 -0400

    Removed recipeBox = scrapy.Field() and some comments

--- scrape/scrapy/recipeScrapy/items.py
+++ scrape/scrapy/recipeScrapy/items.py
@@ -1,13 +1,6 @@
 __author__ = "Rich Johnson"
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-
 
 class findDomainsItem(scrapy.Item):
     domain_name = scrapy.Field()
@@ -16,6 +9,5 @@ class findDomainsItem(scrapy.Item):
 class recipeItem(scrapy.Item):
     url = scrapy.Field()
     title = scrapy.Field()
-    recipe_box = scrapy.Field()
     ingred = scrapy.Field()
     is_indexed = scrapy.Field()
